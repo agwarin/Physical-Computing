d7f3d70b50ad1b236f27393e8c4ef8889f90d465
Niki Selken <nikiselken@gmail.com>
2014-03-17 14:15:17 -0400

    f this

--- LED_Glasses/LED_Glasses.ino
+++ LED_Glasses/LED_Glasses.ino
@@ -3,20 +3,11 @@
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
 
-Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+Adafruit_8x8matrix matrix[2] = {
+  Adafruit_8x8matrix(), Adafruit_8x8matrix()};
+static const uint8_t matrixAddr[] = { 0x70, 0x71};
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("8x8 LED Matrix Test");
-  
-//  matrix.begin(0x70);  // pass in the address
-Adafruit_8x8matrix matrix[2];
 
- for(uint8_t i=0; i<2; i++) {
-    matrix[i] = Adafruit_8x8matrix();
-    matrix[i].begin(0x70 + i);
-  }
-}//setup bracket
 static const uint8_t PROGMEM
    
    blinkleft[] =
@@ -73,83 +64,96 @@ B01111110,
 B00111100};
 
 
+  
+  
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8x8 LED Matrix Test");  delay(1000);
+  
+ for(uint8_t i=0; i<2; i++) {
+    matrix[i] = Adafruit_8x8matrix();
+    matrix[i].begin(0x70 + i);
+  }
+}//setup bracket
+
 void loop() {
   
-   matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(1000);
+//   matrix[0].clear();
+//  matrix[0].drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+//  matrix[0].writeDisplay();
   
-  matrix[0].clear();
-  matrix.drawBitmap(0, 0, left, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-  matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
+//  
+//  matrix[0].clear();
+//  matrix.drawBitmap(0, 0, left, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//  matrix[0].clear();
+//  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//
+//  matrix[0].clear();
+//  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//   matrix[0].clear();
+//  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//
+//  matrix[0].clear();
+//  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//   matrix[0].clear();
+//  matrix.drawBitmap(0, 0, blinkleft, 8, 8, LED_RED);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//  
+//     matrix[0].clear();
+//  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(1000);
+//  
+//  matrix[1].clear();
+//  matrix.drawBitmap(0, 0, left, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//  matrix[1].clear();
+//  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//
+//  matrix[1].clear();
+//  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//   matrix[1].clear();
+//  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//
+//  matrix[1].clear();
+//  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//   matrix[1].clear();
+//  matrix.drawBitmap(0, 0, notblinkright, 8, 8, LED_RED);
+//  matrix.writeDisplay();
+//  delay(500);
+//  
+//
+//  matrix[0].setRotation(0);
+//  matrix[1].setRotation(0);
 
-  matrix[0].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-   matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix[0].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-   matrix[0].clear();
-  matrix.drawBitmap(0, 0, blinkleft, 8, 8, LED_RED);
-  matrix.writeDisplay();
-  delay(500);
-  
-  
-     matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(1000);
-  
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, left, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-   matrix[1].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-   matrix[1].clear();
-  matrix.drawBitmap(0, 0, notblinkright, 8, 8, LED_RED);
-  matrix.writeDisplay();
-  delay(500);
-  
-
-  matrix[0].setRotation(0);
-  matrix[1].setRotation(0);
-}
+}//loop end
 
 
 
