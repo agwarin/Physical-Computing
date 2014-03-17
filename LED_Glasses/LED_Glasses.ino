e221286a7697b08376ef4d658437f0a8374956c7
AnnaKolla <kolla075@newschool.edu>
2014-03-17 14:11:58 -0400

    Both Matrix eyes connected

--- LED_Glasses/LED_Glasses.ino
+++ LED_Glasses/LED_Glasses.ino
@@ -3,20 +3,28 @@
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
 
-Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+Adafruit_BicolorMatrix matrix [2] = {
+  Adafruit_BicolorMatrix(), Adafruit_BicolorMatrix()
+};
+
+static const uint8_t matrixAddr[] = { 0x70, 0x71 };
 
 void setup() {
+  
   Serial.begin(9600);
   Serial.println("8x8 LED Matrix Test");
   
+  
+  
 //  matrix.begin(0x70);  // pass in the address
-Adafruit_8x8matrix matrix[2];
-}
- for(uint8_t i=0; i<2; i++) {
-    matrix[i] = Adafruit_8x8matrix();
-    matrix[i].begin(0x70 + i);
-  }
+//Adafruit_8x8matrix matrix[2];
 
+
+ for(uint8_t i=0; i<2; i++) {
+   // matrix[i] = Adafruit_BicolorMatrix();
+    matrix[i].begin(matrixAddr [i]);
+  }
+}
 static const uint8_t PROGMEM
    
    blinkleft[] =
@@ -30,14 +38,14 @@ B01010100,
 B01010100},
 
 notblinkright[] =
-{B00000000,
-B00000000,
-B00000000,
-B10000001,
-B11000001,
+ {B00111100,
 B01111110,
-B01010100,
-B01010100},
+B11111111,
+B11100111,
+B11100111,
+B11111111,
+B01111110,
+B00111100},
 
 right[] =
 
@@ -75,81 +83,91 @@ B00111100};
 
 void loop() {
   
-   matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(1000);
+  leftEye();
+  rightEye();
   
-  matrix[0].clear();
-  matrix.drawBitmap(0, 0, left, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
+  //for(uint8_t i=0; i<2; i++) matrix[i].writeDisplay();
+
+  delay(20); // ~50 FPS
+
+//  matrix[0].setRotation(0);
+//  matrix[1].setRotation(0);
+}
+
+void leftEye(){
+   matrix[0].clear();
+  matrix[0].drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+  matrix[0].writeDisplay();
   delay(500);
   
   matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
+  matrix[0].drawBitmap(0, 0, left, 8, 8, LED_GREEN);
+  matrix[0].writeDisplay();
+  delay(500);
+  
+  matrix[0].clear();
+  matrix[0].drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+  matrix[0].writeDisplay();
   delay(500);
 
   matrix[0].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
+  matrix[0].drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+  matrix[0].writeDisplay();
   delay(500);
   
    matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
+  matrix[0].drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+  matrix[0].writeDisplay();
   delay(500);
 
   matrix[0].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
+  matrix[0].drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+  matrix[0].writeDisplay();
   delay(500);
   
    matrix[0].clear();
-  matrix.drawBitmap(0, 0, blinkleft, 8, 8, LED_RED);
-  matrix.writeDisplay();
-  delay(500);
+  matrix[0].drawBitmap(0, 0, blinkleft, 8, 8, LED_RED);
+  matrix[0].writeDisplay();
+  delay(50);
   
   
      matrix[0].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
+  matrix[0].drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+ matrix[0].writeDisplay();
   delay(1000);
   
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, left, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-   matrix[1].clear();
-  matrix.drawBitmap(0, 0, look, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix[1].clear();
-  matrix.drawBitmap(0, 0, right, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
-  
-   matrix[1].clear();
-  matrix.drawBitmap(0, 0, notblinkright, 8, 8, LED_RED);
-  matrix.writeDisplay();
-  delay(500);
-  
-
-  matrix[0].setRotation(0);
-  matrix[1].setRotation(0);
 }
 
+void rightEye(){
+    matrix[1].clear();
+  matrix[1].drawBitmap(0, 0, left, 8, 8, LED_GREEN);
+ matrix[1].writeDisplay();
+  delay(500);
+  
+  matrix[1].clear();
+  matrix[1].drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+ matrix[1].writeDisplay();
+  delay(500);
+
+  matrix[1].clear();
+  matrix[1].drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+ matrix[1].writeDisplay();
+  delay(500);
+  
+   matrix[1].clear();
+  matrix[1].drawBitmap(0, 0, look, 8, 8, LED_GREEN);
+ matrix[1].writeDisplay();
+  delay(500);
+
+  matrix[1].clear();
+  matrix[1].drawBitmap(0, 0, right, 8, 8, LED_GREEN);
+  matrix[1].writeDisplay();
+  delay(500);
+  
+   matrix[1].clear();
+  matrix[1].drawBitmap(0, 0, notblinkright, 8, 8, LED_RED);
+  matrix[1].writeDisplay();
+  delay(50);
+}
 
 
