33ad8eb1f34f949f3095960c5a6bdabc9586262a
Niki Selken <nikiselken@gmail.com>
2014-05-06 14:16:28 -0400

    more

--- acceleromerterorama/acceleromerterorama.ino
+++ acceleromerterorama/acceleromerterorama.ino
@@ -118,6 +118,26 @@ void flashRandom(int wait, uint8_t howmany) {
 }// end flash random
 
 
+
+void beep (int speakerPin, float noteFrequency, long noteDuration)
+{    
+  int x;
+  // Convert the frequency to microseconds
+  float microsecondsPerWave = 1000000/noteFrequency;
+  // Calculate how many HIGH/LOW cycles there are per millisecond
+  float millisecondsPerCycle = 1000/(microsecondsPerWave * 2);
+  // Multiply noteDuration * number or cycles per millisecond
+  float loopTime = noteDuration * millisecondsPerCycle;
+  // Play the note for the calculated loopTime.
+  for (x=0;x<loopTime;x++)   
+          {   
+              digitalWrite(speakerPin,HIGH); 
+              delayMicroseconds(microsecondsPerWave); 
+              digitalWrite(speakerPin,LOW); 
+              delayMicroseconds(microsecondsPerWave); 
+          } 
+}  
+
 void scale() 
 {    
           beep(speakerPin, note_C7,500); //C: play the note C for 500ms 
